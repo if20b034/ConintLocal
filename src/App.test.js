68dec34989c51044eae2176f68bd9990b475b951
Yilmaz Thomas <thomasyilmaz1@hotmail.com>
2022-06-16 21:20:12 +0200

    Add Linting

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
+import {React} from 'react';
 
-test('renders learn react link', () => {
+test('1 is one', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(1).toBe(1);
 });
